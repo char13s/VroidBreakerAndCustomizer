474183b3397e22e3e0df4fc88a305b9016efc8c2
char13s <32624173+char13s@users.noreply.github.com>
2024-02-29 22:12:16 -0500

    Clean up and build up
    - Got rid of my personal meshes
    - made a lil camera view of the mesh I'm editing
    - Added a bunch of base character meshes for parts
    - was able to assign clothes to the base mesh
    - made the code able to handle null checks

--- Assets/Editor/VroidBreaker.cs
+++ Assets/Editor/VroidBreaker.cs
@@ -24,27 +24,39 @@ public class VroidBreaker : EditorWindow
     }
     private void ScanObject(GameObject go) {
         string mainName = go.name;
-        GameObject root = go.transform.Find("Root").gameObject;
-        GameObject hair = go.transform.Find("Hair").gameObject;
-        GameObject face = go.transform.Find("Face").gameObject;
-        GameObject body = go.transform.Find("Body").gameObject;
+        GameObject root = null;
+        GameObject hair = null;
+        GameObject face = null;
+        GameObject body = null;
+        if (go.transform.Find("Root"))
+            root = go.transform.Find("Root").gameObject;
+        if (go.transform.Find("Hair"))
+            hair = go.transform.Find("Hair").gameObject;
+        if (go.transform.Find("Face"))
+            face = go.transform.Find("Face").gameObject;
+        if (go.transform.Find("Body"))
+            body = go.transform.Find("Body").gameObject;
         ///---------------------------- might need to come back and save Secondary spring colliders with meshes---------------------------------------------
-        //GameObject secondary = go.transform.Find("Secondary").gameObject;
-        GameObject newHair = new GameObject();
+        //GameObject secondary = go.transform.Find("Secondary").gameObject;    
+        if (hair != null) {
+            GameObject newHair = new GameObject();
         newHair.name = mainName + "'s Hair";
-        GameObject newFace = new GameObject();
+            Instantiate(root, newHair.transform);
+            Instantiate(hair, newHair.transform);
+            PrefabThese(newHair);
+        }
+        if (face != null) {
+            GameObject newFace = new GameObject();
         newFace.name = mainName + "'s Face";
-        Instantiate(root, newHair.transform);
-        Instantiate(hair, newHair.transform);
-        Instantiate(root, newFace.transform);
-        Instantiate(face, newFace.transform);
-        PrefabThese(newHair);
-        PrefabThese(newFace);
+            Instantiate(root, newFace.transform);
+            Instantiate(face, newFace.transform);
+            PrefabThese(newFace);
+        }
         SplitBody(root, body);
     }
     private void PrefabThese(GameObject go) {
-        if (!Directory.Exists("Assets/Prefabs"))
-            AssetDatabase.CreateFolder("Assets", "Prefabs");
+        if (!Directory.Exists("Assets/Resources/MeshParts"))
+            AssetDatabase.CreateFolder("Assets", "Resources");
         string localPath = WhereToGo(go.name);
         localPath = AssetDatabase.GenerateUniqueAssetPath(localPath);
         PrefabUtility.SaveAsPrefabAsset(go, localPath);
@@ -70,6 +82,9 @@ public class VroidBreaker : EditorWindow
     }
     private void SplitBody(GameObject root, GameObject body) {
         Material invisi = Resources.Load("Invisi") as Material;
+        bool createTop = false;
+        bool createBottom = false;
+        bool createShoes = false;
         Debug.Log(invisi);
         List<Material> shoeMats = new List<Material>();
         List<Material> topsMats = new List<Material>();
@@ -79,42 +94,49 @@ public class VroidBreaker : EditorWindow
         foreach (Material mat in gameObjectMats) {
             if (mat.name.Contains("Shoe")) {
                 shoeMats.Add(mat);
+                createShoes = true;
             }
             else {
                 shoeMats.Add(invisi);
             }
             if (mat.name.Contains("Bottom")) {
                 bottomsMats.Add(mat);
+                createBottom = true;
             }
             else {
                 bottomsMats.Add(invisi);
             }
             if (mat.name.Contains("Top") || mat.name.Contains("Onepiece")) {
                 topsMats.Add(mat);
+                createTop = true;
             }
             else {
                 topsMats.Add(invisi);
             }
         }
-        GameObject Top = new GameObject();
-        Top.name = "Tops";
-        Instantiate(root, Top.transform);
-        GameObject top = Instantiate(body, Top.transform);
-        top.GetComponent<SkinnedMeshRenderer>().sharedMaterials = topsMats.ToArray();
-        PrefabThese(Top);
-
-        GameObject Bottom = new GameObject();
-        Bottom.name = "Bottoms";
-        Instantiate(root, Bottom.transform);
-        GameObject bottom = Instantiate(body, Bottom.transform);
-        bottom.GetComponent<SkinnedMeshRenderer>().sharedMaterials = bottomsMats.ToArray();
-        PrefabThese(Bottom);
-
-        GameObject Shoe = new GameObject();
-        Shoe.name = "Shoes";
-        Instantiate(root, Shoe.transform);
-        GameObject shoe = Instantiate(body, Shoe.transform);
-        shoe.GetComponent<SkinnedMeshRenderer>().sharedMaterials = shoeMats.ToArray();
-        PrefabThese(Shoe);
+        if (createTop) {
+            GameObject Top = new GameObject();
+            Top.name = "Tops";
+            Instantiate(root, Top.transform);
+            GameObject top = Instantiate(body, Top.transform);
+            top.GetComponent<SkinnedMeshRenderer>().sharedMaterials = topsMats.ToArray();
+            PrefabThese(Top);
+        }
+        if (createBottom) {
+            GameObject Bottom = new GameObject();
+            Bottom.name = "Bottoms";
+            Instantiate(root, Bottom.transform);
+            GameObject bottom = Instantiate(body, Bottom.transform);
+            bottom.GetComponent<SkinnedMeshRenderer>().sharedMaterials = bottomsMats.ToArray();
+            PrefabThese(Bottom);
+        }
+        if (createShoes) {
+            GameObject Shoe = new GameObject();
+            Shoe.name = "Shoes";
+            Instantiate(root, Shoe.transform);
+            GameObject shoe = Instantiate(body, Shoe.transform);
+            shoe.GetComponent<SkinnedMeshRenderer>().sharedMaterials = shoeMats.ToArray();
+            PrefabThese(Shoe);
+        }
     }
 }
--- Assets/Scripts/CustomizeScripts/CustomizeController.cs
+++ Assets/Scripts/CustomizeScripts/CustomizeController.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,7 +9,7 @@ public class CustomizeController : MonoBehaviour
     List<GameObject> faceSets = new List<GameObject>();
     int faceSetsIndex;
     GameObject currentFace;
-    List<GameObject> topSets =new List<GameObject>();
+    List<GameObject> topSets = new List<GameObject>();
     int topSetsIndex;
     GameObject currentTop;
     List<GameObject> bottomSets = new List<GameObject>();
@@ -19,20 +18,33 @@ public class CustomizeController : MonoBehaviour
     List<GameObject> shoeSets = new List<GameObject>();
     int shoeSetsIndex;
     GameObject currentShoe;
+    [SerializeField] private GameObject hairTarget;
+    [SerializeField] private GameObject faceTarget;
+    [SerializeField] private GameObject topTarget;
+    [SerializeField] private GameObject bottomTarget;
+    [SerializeField] private GameObject shoeTarget;
 
-    public int HairSetsIndex { get => hairSetsIndex; set => Mathf.Clamp(value, 0, hairSets.Count); }
-    public int FaceSetsIndex { get => faceSetsIndex; set => Mathf.Clamp(value, 0, faceSets.Count); }
-    public int TopSetsIndex { get => topSetsIndex; set => Mathf.Clamp(value, 0, topSets.Count); }
-    public int BottomSetsIndex { get => bottomSetsIndex; set => Mathf.Clamp(value, 0, bottomSets.Count); }
-    public int ShoeSetsIndex { get => shoeSetsIndex; set => Mathf.Clamp(value, 0, shoeSets.Count); }
+    public int HairSetsIndex { get => hairSetsIndex; set => hairSetsIndex= Mathf.Clamp(value, 0, hairSets.Count); }
+    public int FaceSetsIndex { get => faceSetsIndex; set => faceSetsIndex=Mathf.Clamp(value, 0, faceSets.Count); }
+    public int TopSetsIndex { get => topSetsIndex; set => topSetsIndex=Mathf.Clamp(value, 0, topSets.Count); }
+    public int BottomSetsIndex { get => bottomSetsIndex; set => bottomSetsIndex=Mathf.Clamp(value, 0, bottomSets.Count); }
+    public int ShoeSetsIndex { get => shoeSetsIndex; set => shoeSetsIndex=Mathf.Clamp(value, 0, shoeSets.Count); }
     public GameObject CurrentHair { get => currentHair; set { currentHair = value; SetHair(); } }
     public GameObject CurrentFace { get => currentFace; set { currentFace = value; SetFace(); } }
     public GameObject CurrentTop { get => currentTop; set { currentTop = value; SetTop(); } }
-    public GameObject CurrentBottom { get => currentBottom; set { currentBottom = value;SetBottom(); } }
+    public GameObject CurrentBottom { get => currentBottom; set { currentBottom = value; SetBottom(); } }
     public GameObject CurrentShoe { get => currentShoe; set { currentShoe = value; SetShoe(); } }
 
     private void Start() {
         GetSets();
+        SetDefaults();
+    }
+    private void SetDefaults() {
+        CurrentHair = hairSets[hairSetsIndex];
+        CurrentFace = faceSets[faceSetsIndex];
+        CurrentTop = topSets[topSetsIndex];
+        CurrentBottom = bottomSets[bottomSetsIndex];
+        CurrentShoe = shoeSets[shoeSetsIndex];
     }
     private void GetSets() {
         foreach (GameObject go in Resources.LoadAll("MeshParts/Tops")) {
@@ -42,7 +54,7 @@ public class CustomizeController : MonoBehaviour
             bottomSets.Add(go);
         }
         foreach (GameObject go in Resources.LoadAll("MeshParts/Hairs")) {
-           hairSets.Add(go);
+            hairSets.Add(go);
         }
         foreach (GameObject go in Resources.LoadAll("MeshParts/Faces")) {
             faceSets.Add(go);
@@ -51,20 +63,30 @@ public class CustomizeController : MonoBehaviour
             shoeSets.Add(go);
         }
     }
-    private void SetHair() { 
-    
+    private void SetHair() {
+        if(hairTarget.transform.childCount>0)
+            DestroyImmediate(hairTarget.transform.GetChild(0).gameObject);
+        Instantiate(currentHair, hairTarget.transform);
     }
     private void SetFace() {
-
+        if (faceTarget.transform.childCount > 0)
+            DestroyImmediate(faceTarget.transform.GetChild(0).gameObject);
+        Instantiate(currentFace, faceTarget.transform);
     }
     private void SetBottom() {
-
+        if (bottomTarget.transform.childCount > 0)
+            DestroyImmediate(bottomTarget.transform.GetChild(0).gameObject);
+        Instantiate(currentBottom, bottomTarget.transform);
     }
     private void SetTop() {
-
+        if (topTarget.transform.childCount > 0)
+            DestroyImmediate(topTarget.transform.GetChild(0).gameObject);
+        Instantiate(currentTop, topTarget.transform);
     }
     private void SetShoe() {
-
+        if (shoeTarget.transform.childCount > 0)
+            DestroyImmediate(shoeTarget.transform.GetChild(0).gameObject);
+        Instantiate(currentShoe, shoeTarget.transform);
     }
     public void CycleThruHair(int val) {
         HairSetsIndex += val;
@@ -99,6 +121,6 @@ public class CustomizeController : MonoBehaviour
         if (ShoeSetsIndex == shoeSets.Count) {
             ShoeSetsIndex = 0;
         }
-        CurrentShoe=shoeSets[shoeSetsIndex];
+        CurrentShoe = shoeSets[shoeSetsIndex];
     }
-    }
+}
